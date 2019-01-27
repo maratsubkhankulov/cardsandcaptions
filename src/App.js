74622d7e0b0f8c6b4843166206af42fff22cbeb6
msubkhankulov <3628920+maratsubkhankulov@users.noreply.github.com>
2019-01-27 09:41:50 -0800

    Respond to card click

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 import { Game } from './game';
 import { fillCardStacks } from './game_room';
 
+import wonka from './img/wonka.jpg';
+
 let game = new Game();
 fillCardStacks(game);
 
 class App extends Component {
+	showAlert() {
+		alert("I'm an alert");
+	}
   render() {
     return (
       <div className="App">
+				<div className="card" onClick={this.showAlert}>
+					<img src={wonka} className="card" alt="logo" />
+				</div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             State: {game.state}
           </p>
