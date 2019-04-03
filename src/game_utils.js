5d17cc6f7ca1ffe74305631e1661e05dfe8e14d4
msubkhankulov <3628920+maratsubkhankulov@users.noreply.github.com>
2019-04-02 21:38:15 -0700

    add gifs and captions

--- src/game_utils.js
+++ src/game_utils.js
@@ -27,24 +27,24 @@ export function fillCardStacks(game) {
 	game.image_stack = [];
 
 	game.addCaptionCard(new CaptionCard(1, "I didn't choose the thug life, the thug life chose me."));
-	game.addCaptionCard(new CaptionCard(2, "Why isn't anybody giving me attention?"));
-	game.addCaptionCard(new CaptionCard(3, "When your bus is late."));
+	game.addCaptionCard(new CaptionCard(2, "When a little kid keeps kicking the back of your seat on the airplane."));
+	game.addCaptionCard(new CaptionCard(3, "When it's time to split the bill."));
 	game.addCaptionCard(new CaptionCard(4, "When you find a dank meme to send to your friends."));
-	game.addCaptionCard(new CaptionCard(5, "Coffee is ready."));
+	game.addCaptionCard(new CaptionCard(5, "When that direct deposit hits."));
 	game.addCaptionCard(new CaptionCard(6, "This isn't even my final form."));
 	game.addCaptionCard(new CaptionCard(7, "Marilize legajuana."));
-	game.addCaptionCard(new CaptionCard(8, "That moment when you find the perfect avocado at the supermarket."));
+	game.addCaptionCard(new CaptionCard(8, "When your ex wants you back."));
 	game.addCaptionCard(new CaptionCard(9, "When you tell your friends that you're vegan."));
-	game.addCaptionCard(new CaptionCard(10, "That face you make when you read a post you don't like."));
-	game.addCaptionCard(new CaptionCard(11, "I didn't choose the thug life, the thug life chose me."));
-	game.addCaptionCard(new CaptionCard(12, "That face you make when your firiend is next to his crush."));
-	game.addCaptionCard(new CaptionCard(13, "When you grind a gear."));
-	game.addCaptionCard(new CaptionCard(14, "When you can't answer question 1."));
-	game.addCaptionCard(new CaptionCard(15, "Just nasty."));
-	game.addCaptionCard(new CaptionCard(16, "When you missed out on tickets."));
-	game.addCaptionCard(new CaptionCard(17, "When someone walks past and they smell really nice."));
+	game.addCaptionCard(new CaptionCard(10, "When your crush responds to your message."));
+	game.addCaptionCard(new CaptionCard(11, "When your credit score goes up."));
+	game.addCaptionCard(new CaptionCard(12, "When your song comes on in the club."));
+	game.addCaptionCard(new CaptionCard(13, "When your phone is on 7%."));
+	game.addCaptionCard(new CaptionCard(14, "When it's on sale and they have your size."));
+	game.addCaptionCard(new CaptionCard(15, "When you spill your drink on your computer."));
+	game.addCaptionCard(new CaptionCard(16, "When someone steps on your fresh kicks."));
+	game.addCaptionCard(new CaptionCard(17, "When you get an unwanted nude."));
 	game.addCaptionCard(new CaptionCard(18, "When you eat sour candy."));
-	game.addCaptionCard(new CaptionCard(19, "The internet is down"));
+	game.addCaptionCard(new CaptionCard(19, "When your Uber/Lyft driver talks too much."));
 	game.addCaptionCard(new CaptionCard(20, "When your mom makes your favourite food."));
 	game.addCaptionCard(new CaptionCard(21, "When you swipe your card and it's declined."));
 	game.addCaptionCard(new CaptionCard(22, "When you see somebody spending money and they owe you money."));
@@ -70,6 +70,19 @@ export function fillCardStacks(game) {
 	game.addImageCard(new ImageCard(19, "./img/19.jpg"));
 	game.addImageCard(new ImageCard(20, "./img/20.jpg"));
 	game.addImageCard(new ImageCard(21, "./img/21.jpg"));
+	game.addImageCard(new ImageCard(22, "./img/omg-bernie.gif"));
+	game.addImageCard(new ImageCard(23, "./img/oprah-ok.gif"));
+	game.addImageCard(new ImageCard(24, "./img/patience.gif"));
+	game.addImageCard(new ImageCard(25, "./img/prince-look.gif"));
+	game.addImageCard(new ImageCard(26, "./img/rihanna-lips.gif"));
+	game.addImageCard(new ImageCard(27, "./img/sad-crying-blackish.gif"));
+	game.addImageCard(new ImageCard(28, "./img/sad-pikachu.gif"));
+	game.addImageCard(new ImageCard(29, "./img/shake-head.gif"));
+	game.addImageCard(new ImageCard(30, "./img/shaq-shimmy.gif"));
+	game.addImageCard(new ImageCard(31, "./img/sponge-bob-thinking.gif"));
+	game.addImageCard(new ImageCard(32, "./img/tyler-damn.gif"));
+	game.addImageCard(new ImageCard(33, "./img/will-smith-surprised.gif"));
+	game.addImageCard(new ImageCard(34, "./img/zoom-suprised.gif"));
 }
 
 export function fillPlayers(game) {
